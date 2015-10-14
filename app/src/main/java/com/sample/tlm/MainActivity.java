c3512796de5642fbdf0afc6a19fc23addb2158ca
Samuel <samuel_jo@edkim.co>
2015-10-14 23:48:36 +0900

    테스트!!

--- app/src/main/java/com/sample/tlm/MainActivity.java
+++ app/src/main/java/com/sample/tlm/MainActivity.java
@@ -5,6 +5,7 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.widget.TextView;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -18,6 +19,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        final TextView tvTitle = (TextView) findViewById(R.id.tvTitle);
+
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -25,9 +28,7 @@ public class MainActivity extends AppCompatActivity {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
 
-//                어려워
-
-
+                tvTitle.setText("하이~~!!!");
             }
         });
     }
