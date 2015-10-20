c3622e48c18636ff84902163c60a5e6855e852d2
Samuel <samuel_jo@edkim.co>
2015-10-20 23:06:09 +0900

    요렇게 하면 조금더 보긴 좋을꺼 같아용.

--- app/src/main/java/com/sample/tlm/MainActivity.java
+++ app/src/main/java/com/sample/tlm/MainActivity.java
@@ -6,14 +6,17 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.TextView;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements OnClickListener{
 
+    TextView tvTitle;
+    FloatingActionButton fab;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,34 +25,52 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        final TextView tvTitle = (TextView) findViewById(R.id.tvTitle);
+         tvTitle = (TextView) findViewById(R.id.tvTitle);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-
-                tvTitle.setText("하이~~!!!");
-            }
-        });
+        fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(this);
+//        fab.setOnClickListener(new OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//
+//                tvTitle.setText("하이~~!!!");
+//            }
+//        });
 
 
       
 
         Button enter = (Button)findViewById(R.id.btnAlarmStart);
-        enter.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        enter.setOnClickListener(this);
+//        enter.setOnClickListener(new OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent i = new Intent(MainActivity.this,AlarmExActivity.class);
+//                startActivity(i);
+//            }
+//        });
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btnAlarmStart:
                 Intent i = new Intent(MainActivity.this,AlarmExActivity.class);
                 startActivity(i);
-            }
-        });
+                break;
+            case R.id.fab:
+                showSnackbar();
+//
+                tvTitle.setText("하이~~!!!");
+                break;
+        }
     }
 
-    public void onClick(View v) {
-  }
+    private void showSnackbar() {
+        Snackbar.make(fab, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show();
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
